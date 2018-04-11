707b2141e1b07f197aa34600eb325ff3b331bc49
Kyle Clarke <32105985+legalmatador@users.noreply.github.com>
2018-04-11 11:16:38 -0400

    SuperCharacter getters
    Made getter methods

--- SuperCharacter.cpp
+++ SuperCharacter.cpp
@@ -20,3 +20,43 @@ bool SuperCharacter::getCanFly() {
 bool SuperCharacter::getHasCape() {
     return hasCape;
 }
+
+bool SuperCharacter::getHasSecretIdentity() {
+	return hasSecretIdentity;
+}
+
+bool SuperCharacter::getUsesTech() {
+	return usesTech;
+}
+
+bool SuperCharacter::getUsesMagic() {
+	return usesMagic;
+}
+
+bool SuperCharcter::getIsAmerican() {
+	return isAmerican;
+}
+
+bool SuperCharacter::getIsFromEarth() {
+	return isFromEarth;
+}
+
+bool SuperCharacter::getHasSuperStrength() {
+	return hasSuperStrength;
+}
+
+bool SuperCharacter::getIsGenius() {
+	return isGenius;
+}
+
+bool SuperCharacter::getKnowsMartialArts() {
+	return knowsMartialArts;
+}
+
+bool SuperCharacter::getIsRich() {
+	return isRich;
+}
+
+bool SuperCharacter::getWearsBlack() {
+	return wearsBlack;
+}
